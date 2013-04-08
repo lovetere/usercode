46494737994aa3894398d5bb13fb726b2dbca595
Maurizio Lo Vetere <Maurizio.Lo.Vetere@cern.ch>
2013-04-08 21:21:06 +0000

    *** empty log message ***

--- HTTrackSeeding/python/TTRHWithTrackAngleButComputeCoarse_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilder_cfi
-
-ttrhWithTrackAngleComputeCoarse = RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilder_cfi.ttrhbwr.clone(
-    ComponentName = cms.string('WithTrackAngleComputeCoarse'),
-    ComputeCoarseLocalPositionFromDisk = cms.bool(True)
-)
-
