1fabcd8b390a4317bc7f7e4b92b435cc7d6ec9f8
Maurizio Lo Vetere <Maurizio.Lo.Vetere@cern.ch>
2013-10-07 01:01:55 +0200

    Bug due to misunderstood fmod fixed

--- HTTrackSeeding/interface/AngularInterval.h
+++ HTTrackSeeding/interface/AngularInterval.h
@@ -8,11 +8,13 @@
   *  \author Maurizio Lo Vetere
   */
 
+#include "DataFormats/GeometryVector/interface/Phi.h"
 #include "MLoVetere/HTTrackSeeding/interface/Interval.h"
 
 #include <algorithm>
 #include <cassert>
 #include <cmath>
+#include <utility>
 #include <vector>
 
 
@@ -21,18 +23,22 @@ class AngularIntervalBase {
   public:
     AngularIntervalBase  ( );
     AngularIntervalBase  ( T inf, T sup );
+    T                         center               ( )                               const;
     IntervalBase<T>           cosRange             ( )                               const;
     bool                      include              ( T angle )                       const;
     bool                      isEmpty              ( )                               const;
     bool                      isFull               ( )                               const;
     T                         length               ( )                               const;
     T                         lower                ( )                               const;
-    T                         upper                ( )                               const;
-    IntervalBase<T>           sinRange             ( )                               const;
+    T                         normalize            ( T angle )                       const;
+    std::pair<IntervalBase<T>,IntervalBase<T>>               
+                              normalize            ( AngularIntervalBase<T> other )  const;
     AngularIntervalBase<T>    operator+            ( AngularIntervalBase<T> other )  const;
     AngularIntervalBase<T>    operator-            ( AngularIntervalBase<T> other )  const;
     AngularIntervalBase<T>    operator-            ( )                               const;
     bool                      overlaps             ( AngularIntervalBase<T> other )  const;
+    IntervalBase<T>           sinRange             ( )                               const;
+    T                         upper                ( )                               const;
     AngularIntervalBase<T> &  turnClockWise        ( T angle );
     AngularIntervalBase<T> &  turnClockWise        ( AngularIntervalBase<T> other );
     AngularIntervalBase<T> &  turnCounterClockWise ( T angle );
@@ -60,9 +66,15 @@ inline  AngularIntervalBase<T>::AngularIntervalBase ( T inf, T sup )
     _mean  = 0.;
   } else {
     _delta = (sup-inf)/2;
-    _mean  = (sup+inf)/2;
-    _mean  = std::fmod(_mean+M_PI,2*M_PI)-M_PI;
+    _mean  = Geom::Phi<T>((sup+inf)/2);
   }
+}
+
+
+template <typename T>
+inline T  AngularIntervalBase<T>::center ( )  const
+{
+  return _mean;
 }
 
 
@@ -83,7 +95,7 @@ inline bool  AngularIntervalBase<T>::include ( T angle )  const
 {
   if ( isEmpty() ) return false;
   if ( isFull () ) return true;
-  angle  = std::fmod(angle+M_PI,2*M_PI)-M_PI;
+  angle = Geom::Phi<T>(angle);
   if ( _mean>angle )
     return ( _mean-_delta<angle ) || ( _mean+_delta>angle+2*M_PI ); 
   else
@@ -120,32 +132,35 @@ inline T  AngularIntervalBase<T>::lower ( )  const
 
 
 template <typename T>
-inline T  AngularIntervalBase<T>::upper ( )  const
-{
-  return _mean+_delta;
-}
-
-
-template <typename T>
-inline bool  AngularIntervalBase<T>::overlaps ( AngularIntervalBase<T> other ) const { 
-  if ( isEmpty() || other.isEmpty() ) return false;
-  if ( isFull () || other.isFull () ) return true;
-  if ( _mean>other._mean )
-    return ( _mean-_delta<other._mean+other._delta ) || ( _mean+_delta>other._mean-other._delta+2*M_PI ); 
+inline T  AngularIntervalBase<T>::normalize ( T angle )  const
+{ 
+  if ( !include(angle) ) return 0.;
+  angle = Geom::Phi<T>(angle);
+  if ( _mean>angle )
+    return    std::max(0.,std::max(angle-_mean+_delta,_mean+_delta-angle-2*M_PI))/(2*_delta); 
   else
-    return ( other._mean-other._delta<_mean+_delta ) || ( other._mean+other._delta>_mean-_delta+2*M_PI ); 
+    return 1.-std::max(0.,std::max(_mean+_delta-angle,angle-_mean+_delta-2*M_PI))/(2*_delta); 
 }
 
 
 template <typename T>
-inline IntervalBase<T>  AngularIntervalBase<T>::sinRange()  const
-{
-  if ( _delta==0. ) return IntervalBase<T>();
-  T max = std::max(sin(_mean-_delta),sin(_mean+_delta));
-  T min = std::min(sin(_mean-_delta),sin(_mean+_delta));
-  if ( include( M_PI/2.) ) max= 1;
-  if ( include(-M_PI/2.) ) min=-1;
-  return IntervalBase<T>(min,max);
+inline std::pair<IntervalBase<T>,IntervalBase<T>>  AngularIntervalBase<T>::normalize ( AngularIntervalBase<T> other )  const
+{ 
+  if ( isEmpty() || other.isEmpty() )
+    return std::pair<IntervalBase<T>,IntervalBase<T>>(IntervalBase<T>()     ,IntervalBase<T>());
+  if ( other.isFull()  )
+    return std::pair<IntervalBase<T>,IntervalBase<T>>(IntervalBase<T>(0.,1.),IntervalBase<T>());
+  IntervalBase<T>  first ( std::min(1.,std::max(0.,(other.lower()-lower()       )/length())) , 
+                           std::min(1.,std::max(0.,(other.upper()-lower()       )/length())) ); 
+  IntervalBase<T>  second( std::min(1.,std::max(0.,(other.lower()-lower()+2*M_PI)/length())) , 
+                           std::min(1.,std::max(0.,(other.upper()-lower()+2*M_PI)/length())) ); 
+  IntervalBase<T>  third ( std::min(1.,std::max(0.,(other.lower()-lower()-2*M_PI)/length())) , 
+                           std::min(1.,std::max(0.,(other.upper()-lower()-2*M_PI)/length())) ); 
+  assert ( second.isEmpty() || third.isEmpty() );
+  if ( third.isEmpty() )
+    return std::pair<IntervalBase<T>,IntervalBase<T>>(first,second);
+  else
+    return std::pair<IntervalBase<T>,IntervalBase<T>>(first,third );
 }
 
 
@@ -176,11 +191,40 @@ inline AngularIntervalBase<T> AngularIntervalBase<T>::operator- ( ) const
 
 
 template <typename T>
+inline bool  AngularIntervalBase<T>::overlaps ( AngularIntervalBase<T> other ) const { 
+  if ( isEmpty() || other.isEmpty() ) return false;
+  if ( isFull () || other.isFull () ) return true;
+  if ( _mean>other._mean )
+    return ( _mean-_delta<other._mean+other._delta ) || ( _mean+_delta>other._mean-other._delta+2*M_PI ); 
+  else
+    return ( other._mean-other._delta<_mean+_delta ) || ( other._mean+other._delta>_mean-_delta+2*M_PI ); 
+}
+
+
+template <typename T>
+inline IntervalBase<T>  AngularIntervalBase<T>::sinRange()  const
+{
+  if ( _delta==0. ) return IntervalBase<T>();
+  T max = std::max(sin(_mean-_delta),sin(_mean+_delta));
+  T min = std::min(sin(_mean-_delta),sin(_mean+_delta));
+  if ( include( M_PI/2.) ) max= 1;
+  if ( include(-M_PI/2.) ) min=-1;
+  return IntervalBase<T>(min,max);
+}
+
+
+template <typename T>
+inline T  AngularIntervalBase<T>::upper ( )  const
+{
+  return _mean+_delta;
+}
+
+
+template <typename T>
 inline AngularIntervalBase<T> &  AngularIntervalBase<T>::turnClockWise ( T angle )
 { 
   if ( isEmpty() || isFull() ) return *this;
-  _mean -= angle;
-  _mean  = std::fmod(_mean+M_PI,2*M_PI)-M_PI;
+  _mean = Geom::Phi<T>(_mean-angle);
   return *this;
 }
 
@@ -202,8 +246,7 @@ template <typename T>
 inline AngularIntervalBase<T> &  AngularIntervalBase<T>::turnCounterClockWise ( T angle )
 { 
   if ( isEmpty() || isFull() ) return *this;
-  _mean += angle;
-  _mean  = std::fmod(_mean+M_PI,2*M_PI)-M_PI;
+  _mean = Geom::Phi<T>(_mean+angle);
   return *this;
 }
 
