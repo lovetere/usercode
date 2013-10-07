02fdd752ac95b209acd93ca0baa5340e23df5b5f
Maurizio Lo Vetere <Maurizio.Lo.Vetere@cern.ch>
2013-10-07 14:27:22 +0200

    Bug fix

--- HTTrackSeeding/interface/HelixParNBins.h
+++ HTTrackSeeding/interface/HelixParNBins.h
@@ -20,10 +20,10 @@ class HelixParNBins : private HelixParBinId
     HelixParNBins (unsigned int ncurv, unsigned int neta, unsigned int nlip, unsigned int nphi, unsigned int ntip )
       : HelixParBinId() { nCurv(ncurv); nEta(neta); nLip(nlip); nPhi(nphi); nTip(ntip); }
     unsigned int  nCurv ( )  const              { return HelixParBinId::nCurv()+1; }
-    unsigned int  nEta  ( )  const              { return HelixParBinId::nCurv()+1; }
-    unsigned int  nLip  ( )  const              { return HelixParBinId::nCurv()+1; }
-    unsigned int  nPhi  ( )  const              { return HelixParBinId::nCurv()+1; }
-    unsigned int  nTip  ( )  const              { return HelixParBinId::nCurv()+1; }
+    unsigned int  nEta  ( )  const              { return HelixParBinId::nEta ()+1; }
+    unsigned int  nLip  ( )  const              { return HelixParBinId::nLip ()+1; }
+    unsigned int  nPhi  ( )  const              { return HelixParBinId::nPhi ()+1; }
+    unsigned int  nTip  ( )  const              { return HelixParBinId::nTip ()+1; }
     void          nCurv ( unsigned int value )  { assert(value!=0); HelixParBinId::nCurv(value-1); }
     void          nEta  ( unsigned int value )  { assert(value!=0); HelixParBinId::nEta (value-1); }
     void          nLip  ( unsigned int value )  { assert(value!=0); HelixParBinId::nLip (value-1); }
